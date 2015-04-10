6f35eb2574bd02ea78bc25d130db79ef18c297f5
Chris James <chris.james@springer.com>
2015-04-10 09:45:52 +0100

    Checking JSON response structure

--- compatability_test.go
+++ compatability_test.go
@@ -8,36 +8,65 @@ import (
 	"testing"
 )
 
-const json = `
+const defaultRequestURI = "/hello"
+
+const jsonFormat = `
 [
     {
         "Name": "Example hello",
         "Request":{
-            "URI" : "/hello",
+            "URI" : %s,
             "Method": "GET"
         },
         "Response":{
             "Code": 200,
-            "Body": "hello, world"
+            "Body": "%s"
         }
     }
 ]
 `
 
-func TestItChecksAValidEndpoint(t *testing.T) {
+func testJSON(responseBody string) string {
+	return fmt.Sprintf(jsonFormat, defaultRequestURI, responseBody)
+}
+
+func TestItChecksAValidEndpointsJSON(t *testing.T) {
+	body := `{"foo":"bar"}`
 	realServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.RequestURI() == "/hello" {
-			fmt.Fprint(w, "hello, world")
+		if r.URL.RequestURI() == defaultRequestURI {
+			fmt.Fprint(w, body)
 		} else {
 			http.Error(w, "Not found", http.StatusNotFound)
 		}
 	}))
 
-	fakeEndPoints, _ := mockingjay.NewFakeEndpoints(json)
+	fakeEndPoints, _ := mockingjay.NewFakeEndpoints(testJSON(body))
 
 	checker := NewCompatabilityChecker(fakeEndPoints)
 
 	if !checker.CheckCompatability(realServer.URL) {
 		t.Error("Checker should've found this endpoint to be correct")
 	}
+}
+
+func TestItFlagsDifferentJSONToBeIncompatible(t *testing.T) {
+	serverResponseBody := `{"foo": "bar"}`
+	fakeResponseBody := `{"baz": "boo"}`
+
+	realServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.RequestURI() == defaultRequestURI {
+			fmt.Fprint(w, serverResponseBody)
+		} else {
+			http.Error(w, "Not found", http.StatusNotFound)
+		}
+	}))
+
+	fakeEndPoints, _ := mockingjay.NewFakeEndpoints(testJSON(fakeResponseBody))
+
+	checker := NewCompatabilityChecker(fakeEndPoints)
+
+	if checker.CheckCompatability(realServer.URL) {
+		t.Error("Checker should've found this endpoint to be incorrect")
+	}
+
 }
