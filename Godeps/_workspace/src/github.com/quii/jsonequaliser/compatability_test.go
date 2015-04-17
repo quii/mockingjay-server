fa982514a6ed7404fe35cad5c133483e9d014a55
paulb <paulb@ashridgetech.com>
2015-04-17 11:56:53 +0100

    Fixed dependencies and start of monkey business

---
+++ Godeps/_workspace/src/github.com/quii/jsonequaliser/compatability_test.go
@@ -0,0 +1,90 @@
+package jsonequaliser
+
+import (
+	"testing"
+)
+
+const simpleJSON = `{"firstname": "chris", "lastname": "james", "age": 30}`
+const comparableJSON = `{"firstname": "christopher", "lastname": "james", "age": 15}`
+const notSimilarJSON = `{"foo":"bar"}`
+
+func TestItKnowsTheSameJSONIsCompatible(t *testing.T) {
+	assertCompatible(t, simpleJSON, simpleJSON)
+}
+
+func TestItKnowsStructurallySameJSONIsCompatible(t *testing.T) {
+	assertCompatible(t, simpleJSON, comparableJSON)
+}
+
+func TestItKnowsDifferentJSONIsIncompatible(t *testing.T) {
+	assertIncompatible(t, simpleJSON, notSimilarJSON)
+}
+
+func TestItDoesntMindSuperflousFieldsInB(t *testing.T) {
+	extraJSON := `{"firstname":"frank", "lastname": "sinatra", "extra field": "blue", "age":70}`
+	assertCompatible(t, simpleJSON, extraJSON)
+}
+
+func TestItReturnsAnErrorForNonJson(t *testing.T) {
+	if _, err := IsCompatible("nonsense", "not json"); err == nil {
+		t.Error("Expected an error to be returned when both json is bad")
+	}
+	if _, err := IsCompatible(simpleJSON, "not json"); err == nil {
+		t.Error("Expected an error to be returned when B is bad json")
+	}
+}
+func TestFloatingPoints(t *testing.T) {
+	floatingJSONa := `{"x": 3.14, "y": "not"}`
+	floatingJSONb := `{"x": "three", "y": "not"}`
+	assertIncompatible(t, floatingJSONa, floatingJSONb)
+}
+
+func TestStringsTypeCheck(t *testing.T) {
+	stringyJSON := `{"x":"y"}`
+	notStringyJSON := `{"x":1}`
+	assertIncompatible(t, stringyJSON, notStringyJSON)
+}
+
+func TestBooleans(t *testing.T) {
+	boolyJSONa := `{"x": true}`
+	boolyJSONb := `{"x": false}`
+	notBoolyJSON := `{"x": 1}`
+	assertCompatible(t, boolyJSONa, boolyJSONb)
+	assertIncompatible(t, boolyJSONa, notBoolyJSON)
+}
+
+func TestItKnowsHowToHandleSimpleArrays(t *testing.T) {
+	JSONWithArray := `{"foo": ["baz", "bo"]}`
+	comparableJSONWithArray := `{"foo": ["bar"]}`
+	badlyTypedJSONArray := `{"foo": [1, 2]}`
+	nonJSONArray := `{"foo":"bar"}`
+
+	assertCompatible(t, JSONWithArray, comparableJSONWithArray)
+	assertIncompatible(t, JSONWithArray, badlyTypedJSONArray)
+	assertIncompatible(t, JSONWithArray, nonJSONArray)
+}
+
+func TestNestedStructures(t *testing.T) {
+	a := `{"hello": [{"x": 1, "y": "a"},{"x": 2, "y": "b"}]}`
+	b := `{"hello": [{"x": 10, "y": "b"}]}`
+	c := `{"hello": [{"x": 10}]}`
+	d := `{"hello": [{"z": 10}]}`
+	e := `{"hello":[1,2,3]}`
+
+	assertCompatible(t, a, b)
+	assertIncompatible(t, a, c)
+	assertIncompatible(t, a, d)
+	assertIncompatible(t, a, e)
+}
+
+func assertCompatible(t *testing.T, a, b string) {
+	if compatible, err := IsCompatible(a, b); !compatible || err != nil {
+		t.Errorf("%s should be compatible with %s (err = %v)", a, b, err)
+	}
+}
+
+func assertIncompatible(t *testing.T, a, b string) {
+	if compatible, err := IsCompatible(a, b); compatible || err != nil {
+		t.Errorf("%s should not be compatible with %s (err = %v)", a, b, err)
+	}
+}
