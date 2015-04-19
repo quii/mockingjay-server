86a2b6bffd4fa30c9d8248d95e27e9e1a98db94d
Chris James <qui666@gmail.com>
2015-04-19 19:59:40 +0100

    Garbage test

--- monkey_test.go
+++ monkey_test.go
@@ -7,28 +7,53 @@ import (
 )
 
 const alwaysMonkeyingAround = 1.0
+const cannedResponse = "hello, world"
 
-func TestItMonkeysWithStatusCodes(t *testing.T) {
-	notFoundBehaviour := new(behaviour)
-	notFoundBehaviour.Frequency = alwaysMonkeyingAround
-	notFoundBehaviour.Status = http.StatusNotFound
+func TestItMonkeysWithStatusCodesAndBodies(t *testing.T) {
+	monkeyBehaviour := new(behaviour)
+	monkeyBehaviour.Frequency = alwaysMonkeyingAround
+	monkeyBehaviour.Status = http.StatusNotFound
+	monkeyBehaviour.Body = "hello, monkey"
 
 	testServer, request := makeTestServerAndRequest()
 
-	monkeyServer := NewMonkeyServer(testServer.Config.Handler, []behaviour{*notFoundBehaviour})
+	monkeyServer := NewMonkeyServer(testServer.Config.Handler, []behaviour{*monkeyBehaviour})
 
 	w := httptest.NewRecorder()
 
 	monkeyServer.ServeHTTP(w, request)
 
-	if w.Code != notFoundBehaviour.Status {
+	if w.Code != monkeyBehaviour.Status {
 		t.Error("Server shouldve returned a 404 because of monkey override")
+	}
+
+	if w.Body.String() != monkeyBehaviour.Body {
+		t.Error("Server should've returned a different body because of monkey override")
+	}
+}
+
+func TestItReturnsGarbage(t *testing.T) {
+	monkeyBehaviour := new(behaviour)
+	monkeyBehaviour.Frequency = alwaysMonkeyingAround
+	monkeyBehaviour.Garbage = 1984
+
+	testServer, request := makeTestServerAndRequest()
+
+	monkeyServer := NewMonkeyServer(testServer.Config.Handler, []behaviour{*monkeyBehaviour})
+
+	w := httptest.NewRecorder()
+
+	monkeyServer.ServeHTTP(w, request)
+
+	if len(w.Body.Bytes()) != monkeyBehaviour.Garbage {
+		t.Error("Server shouldve returned garbage")
 	}
 }
 
 func makeTestServerAndRequest() (*httptest.Server, *http.Request) {
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(cannedResponse))
 	}))
 	request, _ := http.NewRequest("GET", server.URL, nil)
 
