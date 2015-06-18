d676e30be2b3634edc71905c4e26766c9ac3f0b6
Chris James <chris.james@springer.com>
2015-06-18 17:38:37 +0100

    Envrionment variable PORT can now be used to set the port

--- main.go
+++ main.go
@@ -4,10 +4,24 @@ import (
 	"flag"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
-	var port = flag.Int("port", 9090, "Port to listen on")
+
+	envPort := 9090
+	envPortRaw := os.Getenv("PORT")
+	if envPortRaw != "" {
+		i, err := strconv.Atoi(envPortRaw)
+
+		if err != nil {
+			log.Println("Your PORT environment variable isn't an int, defaulting to 9090")
+		} else {
+			envPort = i
+		}
+	}
+
+	var port = flag.Int("port", envPort, "Port to listen on")
 	var configPath = flag.String("config", "", "Path to config YAML")
 	var realURL = flag.String("realURL", "", "Optional: Set this to a URL to check your config against a real server for compatibility")
 	var monkeyConfigPath = flag.String("monkeyConfig", "", "Optional: Set this to add some monkey business")
