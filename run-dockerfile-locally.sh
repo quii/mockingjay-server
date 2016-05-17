45ca1c2ce06a82d2ad49c7062298f4bec33e09cb
Chris James <qui666@gmail.com>
2016-05-17 12:32:22 +0100

    docker problems

--- run-dockerfile-locally.sh
+++ run-dockerfile-locally.sh
@@ -2,4 +2,4 @@
 
 docker build -t mjtest .
 
-docker run --rm=true -p 8080:8080 -v ${PWD}/examples/example.yaml:/example.yaml mjtest -config=/example.yaml -realURL=https://facebook.com -port=8080
+docker run --rm=true -p 8080:8080 -v ${PWD}/examples/example.yaml:/issue42.yaml mjtest -config=/issue42.yaml -realURL=https://facebook.com -port=8080
