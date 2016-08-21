b091ed410aa83d7747dfe82f752c5562514292e1
Chris James <qui666@gmail.com>
2016-08-21 08:23:19 +0100

    dont show body label if there is no body

--- ui/src/client/app/form-controllers/body.jsx
+++ ui/src/client/app/form-controllers/body.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 import { isJSON } from '../util';
 
 function Body({ value, label }) {
-  let text = value || '';
-  if (isJSON(value)) {
-    text = JSON.stringify(JSON.parse(value), null, 2);
+  if (value && value !== '') {
+    let text = value;
+    if (isJSON(value)) {
+      text = JSON.stringify(JSON.parse(value), null, 2);
+    }
+
+    return (
+      <div>
+        <div className="mdl-card__title mdl-card--expand">
+          <h6 className="mdl-card__title-text">{label}</h6>
+        </div>
+        <div className="mdl-card__supporting-text">
+          <pre className="mdl-color-text--primary">{text}</pre>
+        </div>
+      </div>
+    );
   }
 
-  return (
-    <div>
-      <div className="mdl-card__title mdl-card--expand">
-        <h6 className="mdl-card__title-text">{label}</h6>
-      </div>
-      <div className="mdl-card__supporting-text">
-        <pre className="mdl-color-text--primary">{text}</pre>
-      </div>
-    </div>
-  );
+  return null;
 }
 
 Body.propTypes = {
