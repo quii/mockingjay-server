a0ef0a272be4c703dbede6e9587b016eba7de880
Chris James <qui666@gmail.com>
2016-08-23 09:40:23 +0100

    start of service test

--- ui/test/client/app/EndpointService.spec.js
+++ ui/test/client/app/EndpointService.spec.js
@@ -1,7 +1,38 @@
 import assert from 'assert'
+import sinon from 'sinon';
+import EndpointService from '../../../src/client/app/EndpointService'
+
+const api = {
+  getEndpoints: () => {}
+}
 
 describe('Endpoint serverice', () => {
-  it('does things', () => {
-    assert.equal(1, 1)
+
+  let sandbox;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('calls the api to get endpoints', () => {
+
+    const someEndpoints = [
+      {
+        Name: "123"
+      },
+      {
+        Name: "456"
+      },
+    ];
+
+    sandbox.stub(api, 'getEndpoints').returns(Promise.resolve(someEndpoints));
+    const service = new EndpointService(api);
+
+    return service.init()
+      .then(() => assert.equal(true, api.getEndpoints.calledOnce));
   })
 })
