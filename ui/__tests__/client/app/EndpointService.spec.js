9e1d3511bfb225f55003bd473cb477643f27b880
Chris James <qui666@gmail.com>
2016-08-24 18:32:00 +0100

    removed sinon

--- ui/__tests__/client/app/EndpointService.spec.js
+++ ui/__tests__/client/app/EndpointService.spec.js
@@ -1,33 +1,17 @@
 jest.unmock('lodash');
 jest.unmock('bluebird');
-jest.unmock('sinon');
 jest.unmock('../../../src/client/app/EndpointService');
 
 import EndpointService from '../../../src/client/app/EndpointService';
 import _ from 'lodash';
 import Promise from 'bluebird';
-import sinon from 'sinon';
-
-const api = {
-  getEndpoints: () => {
-  },
-  updateEndpoints: () => {
-  },
-};
+import API from '../../../src/client/app/API'
 
 describe('Endpoint serverice', () => {
-  let sandbox;
-
-  beforeEach(() => {
-    sandbox = sinon.sandbox.create();
-  });
-
-  afterEach(() => {
-    sandbox.restore();
-  });
 
 
   it('gets endpoints and then you can select them', () => {
+    const api = new API();
     const service = new EndpointService(api);
 
     const someEndpoints = [
@@ -41,10 +25,10 @@ describe('Endpoint serverice', () => {
       },
     ];
 
-    sandbox.stub(api, 'getEndpoints').returns(Promise.resolve(someEndpoints));
+    api.getEndpoints.mockReturnValueOnce(Promise.resolve(someEndpoints));
 
     return service.init()
-      .then(() => expect(api.getEndpoints.calledOnce).toBe(true))
+      .then(() => expect(api.getEndpoints).toBeCalled())
       .then(() => service.selectEndpoint('123'))
       .then(() => service.getEndpoint())
       .then(endpoint => expect(endpoint.Value).toBe('cat'));
@@ -64,19 +48,20 @@ describe('Endpoint serverice', () => {
       },
     ];
 
+    const api = new API();
     const newEndpointCreator = () => newEndpoint;
     const service = new EndpointService(api, newEndpointCreator);
 
     const mergedEndpoints = _.concat([newEndpoint], someEndpoints);
 
-    sandbox.stub(api, 'getEndpoints').returns(Promise.resolve(someEndpoints));
-    sandbox.stub(api, 'updateEndpoints').returns(Promise.resolve(mergedEndpoints));
+    api.getEndpoints.mockReturnValueOnce(Promise.resolve(someEndpoints));
+    api.updateEndpoints.mockReturnValueOnce(Promise.resolve(mergedEndpoints));
 
 
     return service.init()
       .then(() => service.selectEndpoint('123'))
       .then(() => service.addNewEndpoint())
-      .then(() => expect(api.updateEndpoints.calledWith(JSON.stringify(mergedEndpoints))).toBe(true))
+      .then(() => expect(api.updateEndpoints).toBeCalledWith(JSON.stringify(mergedEndpoints)))
       .then(() => expect(service.endpoints.length).toBe(3))
       .then(() => service.getEndpoint())
       .then(endpoint => expect(endpoint).toBe(newEndpoint));
@@ -95,18 +80,19 @@ describe('Endpoint serverice', () => {
       },
     ];
 
+    const api = new API();
     const service = new EndpointService(api);
 
     const endpointsWithItemDeleted = [someEndpoints[0]];
 
-    sandbox.stub(api, 'getEndpoints').returns(Promise.resolve(someEndpoints));
-    sandbox.stub(api, 'updateEndpoints').returns(Promise.resolve(endpointsWithItemDeleted));
+    api.getEndpoints.mockReturnValueOnce(Promise.resolve(someEndpoints));
+    api.updateEndpoints.mockReturnValueOnce(Promise.resolve(endpointsWithItemDeleted));
 
 
     return service.init()
       .then(() => service.selectEndpoint("456"))
       .then(() => service.deleteEndpoint())
-      .then(() => expect(api.updateEndpoints.calledWith(JSON.stringify(endpointsWithItemDeleted))).toBe(true))
+      .then(() => expect(api.updateEndpoints).toBeCalledWith(JSON.stringify(endpointsWithItemDeleted)))
       .then(() => expect(service.endpoints.length).toBe(1))
       .then(() => service.getEndpoint())
       .then(endpoint => expect(endpoint).toBe(undefined));
