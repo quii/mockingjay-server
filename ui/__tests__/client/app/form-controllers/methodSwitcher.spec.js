0018a31291072c8218bb6631b54be1dedc86f012
Chris James <qui666@gmail.com>
2016-08-25 09:57:29 +0100

    wip

--- ui/__tests__/client/app/form-controllers/methodSwitcher.spec.js
+++ ui/__tests__/client/app/form-controllers/methodSwitcher.spec.js
@@ -1,23 +1,41 @@
 jest.unmock('../../../../src/client/app/form-controllers/methodSwitcher.jsx');
 import MethodSwitcher from '../../../../src/client/app/form-controllers/methodSwitcher.jsx';
 
+import _ from 'lodash';
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 
 describe('Method switcher', () => {
 
-  it('changes the highlight as users click', () => {
+    it('changes the highlights the selected method', () => {
 
-    const onChange = jest.fn();
-    const component = TestUtils.renderIntoDocument(<MethodSwitcher selected="POST" onChange={onChange} />);
+        const initiallySelectedMethod = "POST";
+        const onChange = jest.fn();
+        const component = TestUtils.renderIntoDocument(<MethodSwitcher selected={initiallySelectedMethod}
+                                                                       onChange={onChange}/>);
+        const buttons = TestUtils.scryRenderedDOMComponentsWithTag(component, "button");
+        const buttonNames = buttons.map(b => b.textContent);
 
-    const renderedButtons = TestUtils.scryRenderedDOMComponentsWithTag(component, "button");
-    const buttonNames = renderedButtons.map(b => b.textContent);
+        expect(buttonNames).toEqual(MethodSwitcher.methods);
+        expect(getSelectedMethod(component)).toEqual(initiallySelectedMethod);
 
-    expect(buttonNames).toEqual(MethodSwitcher.methods);
+        const putButton = findButtonWithMethod(buttons, 'PUT');
+        TestUtils.Simulate.click(putButton);
 
-    const selectedButton = TestUtils.findRenderedDOMComponentWithClass(component, MethodSwitcher.selectedCSS);
-
-    expect(selectedButton.textContent).toEqual("POST")
-  });
+        expect(onChange).toBeCalledWith(
+            {
+                target: {
+                    name: 'method',
+                    value: 'PUT',
+                }
+            });
+    });
 });
+
+function findButtonWithMethod(buttons, method) {
+    return _.find(buttons, b => b.textContent === method)
+}
+
+function getSelectedMethod(component) {
+    return TestUtils.findRenderedDOMComponentWithClass(component, MethodSwitcher.selectedCSS).textContent;
+}
