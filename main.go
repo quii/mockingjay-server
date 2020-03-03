30ae249c6ffef88cb21de72e065bb697d3575bfa
Chris James <qui666@gmail.com>
2020-03-03 13:27:55 +0000

    add some timeouts, have a panic handler

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -24,11 +25,38 @@ func main() {
 			log.Fatal(err)
 		} else {
 			config.logger.Printf("Listening on port %d", config.port)
-			err = http.ListenAndServe(fmt.Sprintf(":%d", config.port), svr)
+			webServer := newServer(svr, config.port)
+			err = webServer.ListenAndServe()
 			if err != nil {
 				msg := fmt.Sprintf("There was a problem starting the mockingjay server on port %d: %s", config.port, err.Error())
 				config.logger.Fatal(msg)
 			}
 		}
 	}
+}
+
+func newServer(router http.Handler, port int) *http.Server {
+	return &http.Server{
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       30 * time.Second,
+		ReadHeaderTimeout: 20 * time.Second,
+		Handler:           http.TimeoutHandler(Recovery(router), 5*time.Second, "Timed out!"),
+		Addr:              fmt.Sprintf(":%d", port),
+	}
+}
+
+func Recovery(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			err := recover()
+			if err != nil {
+				msg := fmt.Sprintf("Oh dear, mockingjay has had a panic %#v", err)
+				log.Println(msg)
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Fprint(w, msg)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
 }
